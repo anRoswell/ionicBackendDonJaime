7f4e995465f0e9547689512395f0fd5bc929d587
Alfonso <alfonso.navarro@syspotec.com>
2020-04-25 23:07:24 -0500

    Nueva ruta, books

--- app.js
+++ app.js
@@ -7,67 +7,16 @@ const PORT = 3001
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ extended: true }))
 
+app.get('/', (req, res) => {
+	res.json({ status: 'success' })
+})
+
 const routes = require('./routing/routes')
 app.use('/api/v1.0/', routes)
 
-/**************************************+*/
-// const bookDB = new DB('BOOKS')
-
-// app.get('/books/:id', async (req, res) => {
-// 	try {
-// 		const {
-// 			params: { id },
-// 		} = req
-// 		const [users] = await bookDB.getOne(id)
-// 		res.json({ users })
-// 	} catch (error) {
-// 		res.json({ error })
-// 	}
-// })
-
-// app.get('/books', async (req, res) => {
-// 	try {
-// 		const books = await bookDB.getAll()
-// 		res.json({ books })
-// 	} catch (error) {
-// 		res.json({ error })
-// 	}
-// })
-
-// app.post('/books', async (req, res) => {
-// 	try {
-// 		const data = req.body
-// 		const response = await bookDB.create(data)
-// 		res.json({ status: 'ok', response })
-// 	} catch (error) {
-// 		res.json({ status: 'error', error })
-// 	}
-// })
-
-// app.put('/books/:id', async (req, res) => {
-// 	try {
-// 		const { body: data } = req
-// 		const {
-// 			params: { id },
-// 		} = req
-// 		const response = await bookDB.update(data, id)
-// 		res.json({ status: 'ok', response })
-// 	} catch (error) {
-// 		res.json({ status: 'error', error })
-// 	}
-// })
-
-// app.delete('/books/:id', async (req, res) => {
-// 	try {
-// 		const {
-// 			params: { id },
-// 		} = req
-// 		const response = await bookDB.destroy(id)
-// 		res.json({ status: 'ok', response })
-// 	} catch (error) {
-// 		res.json({ status: 'error', error })
-// 	}
-// })
+app.get('*', (req, res) => {
+	rres.status(401).json({ status: 'error' })
+})
 
 app.listen(PORT, () => {
 	console.log(`Servidor escuchando en el puerto ${PORT}`)
--- routing/routes.js
+++ routing/routes.js
@@ -1,4 +1,5 @@
 const users = require('./routes/users')
 const lenguajes = require('./routes/lenguajes')
+const books = require('./routes/books')
 
-module.exports = [users, lenguajes]
+module.exports = [users, lenguajes, books]
